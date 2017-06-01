d01eed6ea4473a1677bd6d76ffc7b8072ea00e05
MR.YUAN <mryuan0428@126.com>
2017-06-01 20:35:05 +0800

    upload

---
+++ bigInt.h
@@ -0,0 +1,41 @@
+#ifndef BIGINT_H
+#define BIGINT_H
+#include <string>
+#include <iostream>
+
+class bigInt
+{
+private:
+	std::string num;
+public:
+	std::string getnum() const;
+	bool setnum(std::string s);
+	void print() const;
+	bigInt operator * (const bigInt&) const;
+	bigInt operator / (const bigInt&) const;
+	bigInt operator % (const bigInt&) const;
+	bigInt operator + (const bigInt&) const;
+	bigInt operator - (const bigInt&) const;
+	bool operator == (const bigInt&) const;
+	bool operator > (const bigInt&) const;
+	bool operator < (const bigInt&) const;
+	bool operator >=  (const bigInt&) const;
+	bool operator <= (const bigInt&) const;
+    bigInt& operator = (const bigInt&);
+	friend std::ostream& operator << (std::ostream&, const bigInt&);
+    friend std::istream& operator >> (std::istream&, bigInt&);
+	bigInt(std::string);
+	bigInt(const bigInt &);
+    bigInt();
+    std::string getnum()
+    {
+        return num;
+    }
+};
+
+std::string toHex(std::string);
+std::string toDec(std::string);
+bigInt half(bigInt b);
+
+#endif
+
